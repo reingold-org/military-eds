272c5dab061dca3a5b23b0a2e92f2e50838caf47
Jeff Foxx <foxx@adobe.com>
2025-11-19 19:41:05 -0500

    remove build auto block hero

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -89,7 +89,7 @@ function buildAutoBlocks(main) {
       });
     }
 
-    buildHeroBlock(main);
+    //buildHeroBlock(main);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
